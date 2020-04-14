eb84fd4401d29f9a95cd67c22a83b6e5bf294ebe
Gilson Cavalcanti <gilsoncav@gmail.com>
2020-04-14 16:19:16 -0300

    Added SASS support and styled the suits color and card fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
-import './App.css';
+import './App.scss';
 
+export const kSPADES = 'S';
+export const kCLUBS = 'C';
+export const kHEARTS = 'H';
+export const kDIAMONDS = 'D';
+export const kJACK = 'J';
+export const kQUEEN = 'Q';
+export const kKING = 'K';
 
-class Deck {
+class DeckModel {
     constructor() {
-        this._kCardFaces = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
-        this._kSuits = ['S', 'C', 'H', 'D'];
+        this._kCardFaces = ['1', '2', '3', '4', '5', '6', '7', '8', '9', kJACK, kQUEEN, kKING];
+        this._kSuits = [kSPADES, kCLUBS, kHEARTS, kDIAMONDS];
         this._plainDeck = this._generatePlainDeck();
         this._shuffledDeck = this._shuffle(this._shuffledDeck);
     }
@@ -14,7 +21,7 @@ class Deck {
         const deck = [];
 
         this._kSuits.forEach((suit) => {
-            this._kCardFaces.forEach((face) => deck.push({face, suit}));
+            this._kCardFaces.forEach((face) => deck.push(new CardModel(face, suit)));
         })
         return deck;
     }
@@ -43,34 +50,51 @@ class Deck {
 
 }
 
+class CardModel {
+    constructor(face, suit) {
+        this.face = face;
+        this.suit = suit;
+    }
+
+    get isRedSuit() {
+        return this.suit === kHEARTS || this.suit === kDIAMONDS;
+    }
+
+    get isBlackSuit() {
+        return this.suit === kCLUBS || this.suit === kSPADES;
+    }
+}
+
 const CardsTable = () => {
 
-    const [deck] = useState(new Deck());
-    const [cards, setCards]  = useState(deck.plainDeck);
+    const [deck] = useState(new DeckModel());
+    const [cards]  = useState(deck.plainDeck);
 
     return (
         <div className='CardsTable'>
             {cards.map((card) =>(
-                <Card face={card.face} suit={card.suit}>
+                <Card cardModel={card}>
                 </Card>
             ))}
         </div>
     )
 }
 
-const Card = ({face, suit}) => {
+const Card = ({cardModel}) => {
     return (
-        <div className='Card'>
-            <span className='Card-topSuit'>{suit}</span>
-            <span className='Card-face'>{face}</span>
-            <span className='Card-bottomSuit'>{suit}</span>
+        <div className={cardModel.isRedSuit ? 'red-suit' : 'black-suit'}>
+            <span className='top-suit'>{cardModel.suit}</span>
+            <span className='face'>{cardModel.face}</span>
+            <span className='bottom-suit'>{cardModel.suit}</span>
         </div>
     )
 }
 
 const App = () => {
     return (
-        <CardsTable />
+        <div className='App'>
+            <CardsTable />
+        </div>
     )
 }
 
